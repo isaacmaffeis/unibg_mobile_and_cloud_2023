7a7104d74d0a2d13d60381dfbb01ee8731740491
Isaac <isaacmaffo96@gmail.com>
2023-05-30 18:17:57 +0200

    nodeJs

--- NodeJs/serverDb.js
+++ NodeJs/serverDb.js
@@ -19,63 +19,60 @@ const server = http.createServer(function (req, res) {
   var myurl = new url.URL("http://"+req.headers.host + req.url);
   var params = myurl.searchParams;
 
-var client_config =
-{ useUnifiedTopology: true,
-  useNewUrlParser: true };
+  var client_config = { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true 
+  };
 
-const client = new mongo.MongoClient(db_url, client_config)
+  const client = new mongo.MongoClient(db_url, client_config)
 
-// Connect to the db
-client.connect(async function(err) {
- if(err) 
- {
-    console.log("Error connecting to MongoDB");
-    throw err;
- }
- db = await client.db(db_name);
-
- if( myurl.pathname == "/insert" )
- {
-   var o = new Object();
-   for (const [name, value] of params) 
-   {
-    o[name] = value;
-   }
-  var collection = await db.collection(collection_name);
-  var r = await collection.insertOne( o );
-
-  if(r.result.ok == 1 )
-  {
-   output_text = JSON.stringify( {inserted: 1 } );
-   MIME = MIME_json;
+  // Connect to the db
+  client.connect(async function(err) {
+  if(err) {
+      console.log("Error connecting to MongoDB");
+      throw err;
   }
-  else
-  {
-   output_text = JSON.stringify( r );
-   MIME = MIME_json;
+
+  db = await client.db(db_name);
+
+  if( myurl.pathname == "/insert" ) {
+    var o = new Object();
+
+    for (const [name, value] of params) {
+      o[name] = value;
+    }
+
+    var collection = await db.collection(collection_name);
+    var r = await collection.insertOne( o );
+
+    if(r.result.ok == 1 ) {
+    output_text = JSON.stringify( {inserted: 1 } );
+    MIME = MIME_json;
+    } 
+    else {
+    output_text = JSON.stringify( r );
+    MIME = MIME_json;
+    }
   }
- }
 
- if( myurl.pathname == "/list" )
- {
-  var collection = await db.collection(collection_name);
-  const docs = await collection.find().limit(10).toArray();
-  const total = await collection.find().count();
-  var o = new Object();
-  o.Total = total;
-  o.Selected = 10;
-  o.docs = docs;
+  if( myurl.pathname == "/list" ) {
+    var collection = await db.collection(collection_name);
+    const docs = await collection.find().limit(10).toArray();
+    const total = await collection.find().count();
+    var o = new Object();
+    o.Total = total;
+    o.Selected = 10;
+    o.docs = docs;
+    output_text = JSON.stringify( o );
+    MIME = MIME_json;
+  }
 
-  output_text = JSON.stringify( o );
-  MIME = MIME_json;
- }
+    client.close();
 
-  client.close();
-
-  res.statusCode = 200
-  res.setHeader('Content-Type', MIME)
-  res.end(output_text);     
-});
+    res.statusCode = 200
+    res.setHeader('Content-Type', MIME)
+    res.end(output_text);     
+  });
 
 }); // callback di HTTP server
 
