3ce808b1cf8fc8b23f76939cf6cb03dcc98b5e77
Isaac <isaacmaffo96@gmail.com>
2023-06-08 18:46:57 +0200

    Javascript exes

--- Javascript/gioco.js
+++ Javascript/gioco.js
@@ -1,3 +1,10 @@
+ /* Si crei la funzione Gioco, che funge da costruttore di oggetti con le seguenti caratteristiche: 
+    1. Propetà degli oggetti: Codice, Titolo, strumento
+    2. Numero di giocatori: o un numero secco, oppure un oggetto con due campi minimo e massimo
+    3. Metodi: setCodice, setTitolo, setStrumento, setGiocatori, genHTML (genera una rappresentazione
+       semplificata del gioco in HTML, a vostra scelta, che inserisce in un elemento della
+       pagina di cui riceve il valore dell'id) */
+ 
  function Gioco(){
 
     this.codice = null;
@@ -5,7 +12,7 @@
     this.strumento = null;
 
     this.numero_giocatori = function(numero_giocatori){
-        if (typeof(numero_giocatori)== "number")
+        if (typeof(numero_giocatori) == "number")
             this.giocatori = numero_giocatori;
         else{
             this.minimo = numero_giocatori[0];
@@ -25,9 +32,12 @@
         this.strumento = strumento;
     }
 
+    this.setGiocatori = function(numero_giocatori){
+        this.numero_giocatori = numero_giocatori;
+    }
+
     this.genHTML = function(ID){
         var node = document.getElementById(ID);
         node.innerHTML = JSON.stringify(this)
     }
  }
- 
